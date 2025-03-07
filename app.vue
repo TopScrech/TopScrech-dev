c6934eed3977131ac6204b569883336039f90db4
TopScrech <sergei_saliukov@icloud.com>
2025-03-07 12:11:26 +0100

    Dynamic theme

--- app.vue
+++ app.vue
@@ -1,10 +1,24 @@
 <script setup lang="ts">
 import MyComponent from '~/MyComponent.vue'
+import '~/main.css'
 </script>
 
 <template>
-  <div>
-    <h1>Welcome</h1>
+  <div class="theme">
     <MyComponent/>
   </div>
 </template>
+
+<style scoped>
+.theme {
+  background-color: white;
+  color: black;
+}
+
+@media (prefers-color-scheme: dark) {
+  .theme {
+    background-color: black;
+    color: white;
+  }
+}
+</style>
