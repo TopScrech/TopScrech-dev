85684da92673962c2364751afd297a8891747e56
TopScrech <sergei_saliukov@icloud.com>
2025-03-07 10:26:12 +0100

    -

--- MyComponent.vue
+++ MyComponent.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import MyComponent from '~/MyComponent.vue'
+
 </script>
 
 <template>
--- app.vue
+++ app.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+import MyComponent from '~/MyComponent.vue'
+</script>
+
 <template>
   <div>
     <h1>Welcome</h1>
